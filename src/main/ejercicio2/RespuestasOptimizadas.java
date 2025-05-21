64156a2f4b87651befbd4ddcb8dbe676eb24c948
Brandon <bcollaguazos@est.ups.edu.ec>
2025-05-21 07:32:25 -0500

    Ejericicio 1 y 2

--- src/main/ejercicio2/RespuestasOptimizadas.java
+++ src/main/ejercicio2/RespuestasOptimizadas.java
@@ -1,5 +1,8 @@
 package main.ejercicio2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RespuestasOptimizadas {
     // El estudiante debe escribir aquí las versiones optimizadas y las
     // complejidades
@@ -8,7 +11,13 @@ public class RespuestasOptimizadas {
     public static String tieneDuplicadosComplejidad = ""; // Ej: "O(n)"
 
     public static boolean tieneDuplicadosOpt(int[] arr) {
-        // TODO: implementar versión eficiente
+        Set<Integer> vistos = new HashSet<>();
+        for (int n : arr) {
+            if (vistos.contains(n)) {
+                return true;
+            }
+            vistos.add(n);
+        }
         return false;
     }
 
@@ -16,15 +25,30 @@ public class RespuestasOptimizadas {
     public static String contarMayoresComplejidad = ""; // Ej: "O(n)"
 
     public static int contarMayoresOpt(int[] arr, int x) {
-        // TODO: implementar versión eficiente
-        return 0;
+        int count = 0;
+        int maxAnterior = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > x && arr[i] >= maxAnterior) {
+                count++;
+            }
+            if (arr[i] > maxAnterior) {
+                maxAnterior = arr[i];
+            }
+        }
+        return count;
     }
+
 
     // 3. Versión optimizada de encontrarMaximo (Complejidad anotada en la variable)
     public static String encontrarMaximoComplejidad = ""; // Ej: "O(n)"
 
     public static int encontrarMaximoOpt(int[] arr) {
-        // TODO: implementar versión eficiente
-        return 0;
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
     }
 }
